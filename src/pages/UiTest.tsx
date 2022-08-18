99b06583364ade8ec609296e1968dc7297202857
Denys Shevel <denys.shevel16@gmail.com>
2022-08-18 06:26:15 -0400

    Define project structure and add router

---
+++ src/pages/UiTest.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ui } from "../components";
+
+const UiTest: React.FC = () => {
+    return (
+        <div>
+            <ui.Button></ui.Button>
+        </div>
+    );
+};
+
+export default UiTest;
