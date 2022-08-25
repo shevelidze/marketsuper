c7a6cf2f351b38d4fc6d7a5f428857851c215a9f
Denys Shevel <denys.shevel16@gmail.com>
2022-08-25 10:25:02 -0400

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import UiTest from './pages/UiTest';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/uitest" element={<UiTest />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/uitest" element={<UiTest />} />
+      </Routes>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../Logo';
+import styles from './Footer.module.css';
+
+const Footer: React.FC = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.section}>
+        <Logo />
+        <div>Copyright © 2022 Marketsuper Inc.</div>
+      </div>
+      <div className={styles.section}>
+        <div>Kyiv, Ukraine</div>
+        <div>support@marketsuper.ua</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
